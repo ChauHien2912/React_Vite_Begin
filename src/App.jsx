7afca00c934407042668c41d8a515a70c0107e0a
chauh <Chauhien2912@gmail.com>
2025-07-05 22:49:16 +0700

    funct

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 const App = () => {
-
-
+  const addNewTodo = () => {
+    alert("call me")
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
-      <TodoNew />
-      <TodoData />
+      <TodoNew
+        addNewTodo={addNewTodo}
+      />
+      <TodoData
+      />
       <div className='todo-image'>
         <img src={reactLogo} className='logo' />
       </div>
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,8 +1,17 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log("<<< check log:", props)
+    const { addNewTodo } = props;
+    //addNewTodo()
+    const handleClick = () => {
+        alert("click me")
+    }
     return (
         <div className='todo-new'>
             <input type="text" />
-            <button>Add</button>
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
